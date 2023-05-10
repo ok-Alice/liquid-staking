7a759897516fb142a6188e7de3b189df277bcc69
Michael Assaf <michael.antoine.assaf@gmail.com>
2023-05-10 12:32:16 -0400

    update scripts

--- .devcontainer/scripts/rust-setup.sh
+++ .devcontainer/scripts/rust-setup.sh
@@ -4,6 +4,8 @@
 echo "[net]" > $CARGO_HOME/config.toml
 echo "git-fetch-with-cli = true" >> $CARGO_HOME/config.toml
 
+rustup install stable-x86_64-unknown-linux-gnu
+
 rustup default stable
 rustup update
 rustup update nightly
--- .devcontainer/scripts/swanky-setup.sh
+++ .devcontainer/scripts/swanky-setup.sh
@@ -6,8 +6,9 @@ if [ ! -d "$swanky_folder" ]; then
   sudo mkdir -p "$swanky_folder"
 fi
 
-# wget -O /tmp/swanky.tar.gz https://github.com/AstarNetwork/swanky-cli/releases/download/v2.1.1/swanky-v2.1.1-32e0874-linux-x64.tar.gz && sudo tar -xf /tmp/swanky.tar.gz -C /opt
+
 wget -O /tmp/swanky.tar.gz https://github.com/AstarNetwork/swanky-cli/releases/download/v2.1.2/swanky-v2.1.2-25dc2ae-linux-x64.tar.gz && sudo tar -xf /tmp/swanky.tar.gz -C /opt
+#wget -O /tmp/swanky.tar.gz https://github.com/AstarNetwork/swanky-cli/releases/download/v2.1.2/swanky-v2.1.2-25dc2ae-darwin-arm64.tar.gz && sudo tar -xf /tmp/swanky.tar.gz -C /opt
 
 link_path="/usr/local/bin/swanky"
 swanky_bin_path="/opt/swanky/bin/swanky"
