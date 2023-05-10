e7e36c320c32ebd9559473c8be6789ecbc03282c
Snowmead <michael.assaf.edge@gmail.com>
2023-05-10 11:44:20 -0400

    update scripts

---
+++ .devcontainer/scripts/rust-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Fix memory leakage from aarm64 mac
+echo "[net]" > $CARGO_HOME/config.toml
+echo "git-fetch-with-cli = true" >> $CARGO_HOME/config.toml
+
+rustup default stable
+rustup update
+rustup update nightly
+rustup component add rust-src
+rustup component add rust-src --toolchain nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup default nightly
+
+cargo install cargo-dylint dylint-link
+cargo install cargo-contract --force --locked
+
+rustup component add rust-src --toolchain stable-x86_64-unknown-linux-gnu
+
+sudo apt install jq
+sudo apt reinstall libc-bin
