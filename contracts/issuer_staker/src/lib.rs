e27d847c6ba5f34e46eeab09e003bc8b25fe5940
Snowmead <michael.assaf.edge@gmail.com>
2023-06-12 11:30:34 -0400

    add set code hash for upgrades, and add pausable macro to bond nomination pool method

--- contracts/issuer_staker/src/lib.rs
+++ contracts/issuer_staker/src/lib.rs
@@ -48,6 +48,21 @@ pub mod issuer_staker {
         pub fn change_state(&mut self) -> Result<(), PausableError> {
             self._switch_pause()
         }
+
+        /// Modifies the code which is used to execute calls to this contract address
+        /// (`AccountId`).
+        ///
+        /// We use this to upgrade the contract logic. We don't do any authorization here,
+        /// any caller can execute this method.
+        ///
+        /// In a production contract you would do some authorization here!
+        #[ink(message)]
+        pub fn set_code(&mut self, code_hash: Hash) {
+            self.env().set_code_hash(&code_hash).unwrap_or_else(|err| {
+                panic!("Failed to `set_code_hash` to {code_hash:?} due to {err:?}")
+            });
+            ink::env::debug_println!("Switched code hash to {:?}.", code_hash);
+        }
     }
 
     impl Pausable for IssuerStaker {
@@ -60,44 +75,45 @@ pub mod issuer_staker {
     impl IssuerStaker {
         #[ink(message)]
         pub fn read_current_era(&self) -> u32 {
-            0u32
+            unimplemented!("read_current_era not implemented")
         }
 
         #[ink(message)]
         pub fn read_unbonding_period(&self) -> u32 {
-            0u32
+            unimplemented!("read_unbonding_period not implemented")
         }
 
         #[ink(message)]
-        pub fn read_era_reward(&self, era: u32) -> Result<(), NPSError> {
-            Ok(())
+        pub fn read_era_reward(&self, _era: u32) -> Result<(), NPSError> {
+            unimplemented!("read_era_reward not implemented")
         }
 
         #[ink(message)]
-        pub fn read_era_staked(&self, era: u32) -> Result<(), NPSError> {
-            Ok(())
+        pub fn read_era_staked(&self, _era: u32) -> Result<(), NPSError> {
+            unimplemented!("read_era_staked not implemented")
         }
 
         #[ink(message)]
-        pub fn read_staked_amount(&self, account: AccountId) -> Result<(), NPSError> {
-            Ok(())
+        pub fn read_staked_amount(&self, _account: AccountId) -> Result<(), NPSError> {
+            unimplemented!("read_staked_amount not implemented")
         }
 
         #[ink(message)]
         pub fn read_staked_amount_on_contract(
             &self,
-            staker: AccountId,
-            contract: AccountId,
+            _staker: AccountId,
+            _contract: AccountId,
         ) -> Result<(), NPSError> {
-            Ok(())
+            unimplemented!("read_staked_amount_on_contract not implemented")
         }
 
         #[ink(message)]
-        pub fn read_contract_stake(&self, account: AccountId) -> Result<(), NPSError> {
-            Ok(())
+        pub fn read_contract_stake(&self, _account: AccountId) -> Result<(), NPSError> {
+            unimplemented!("read_contract_stake not implemented")
         }
 
         #[ink(message, payable)]
+        #[openbrush::modifiers(when_not_paused)]
         pub fn bond_nomination_pool(&mut self) -> Result<(), NPSError> {
             // make sure the caller is recorded as staker
 
@@ -115,38 +131,38 @@ pub mod issuer_staker {
         }
 
         #[ink(message)]
-        pub fn unbond_and_unstake(&mut self, value: Balance) -> Result<(), NPSError> {
-            Ok(())
+        pub fn unbond_and_unstake(&mut self, _value: Balance) -> Result<(), NPSError> {
+            unimplemented!("unbond_and_unstake not implemented")
         }
 
         #[ink(message)]
         pub fn withdraw_unbonded(&mut self) -> Result<(), NPSError> {
-            Ok(())
+            unimplemented!("withdraw_unbonded not implemented")
         }
 
         #[ink(message)]
-        pub fn claim_dapp(&mut self, account_id: AccountId, era: u32) -> Result<(), NPSError> {
-            Ok(())
+        pub fn claim_dapp(&mut self, _account_id: AccountId, era: u32) -> Result<(), NPSError> {
+            unimplemented!("claim_dapp not implemented")
         }
 
         #[ink(message)]
         pub fn claim_staker(&mut self) -> Result<(), NPSError> {
-            Ok(())
+            unimplemented!("claim_staker not implemented")
         }
 
         #[ink(message)]
-        pub fn set_reward_destination(&mut self, destination: u8) -> Result<(), NPSError> {
-            Ok(())
+        pub fn set_reward_destination(&mut self, _destination: u8) -> Result<(), NPSError> {
+            unimplemented!("set_reward_destination not implemented")
         }
 
         #[ink(message)]
         pub fn nomination_transfer(
             &mut self,
-            origin_contract: AccountId,
-            target_contract: AccountId,
-            value: Balance,
+            _origin_contract: AccountId,
+            _target_contract: AccountId,
+            _value: Balance,
         ) -> Result<(), NPSError> {
-            Ok(())
+            unimplemented!("nomination_transfer not implemented")
         }
     }
 
@@ -270,5 +286,69 @@ pub mod issuer_staker {
 
             Ok(())
         }
+
+        // #[ink_e2e::test(additional_contracts = "./updated_incrementer/Cargo.toml")]
+        // async fn set_code_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+        //     // Given
+        //     let constructor = IncrementerRef::new();
+        //     let contract_acc_id = client
+        //         .instantiate("incrementer", &ink_e2e::alice(), constructor, 0, None)
+        //         .await
+        //         .expect("instantiate failed")
+        //         .account_id;
+
+        //     let get = build_message::<IncrementerRef>(contract_acc_id.clone())
+        //         .call(|incrementer| incrementer.get());
+        //     let get_res = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
+        //     assert!(matches!(get_res.return_value(), 0));
+
+        //     let inc = build_message::<IncrementerRef>(contract_acc_id.clone())
+        //         .call(|incrementer| incrementer.inc());
+        //     let _inc_result = client
+        //         .call(&ink_e2e::alice(), inc, 0, None)
+        //         .await
+        //         .expect("`inc` failed");
+
+        //     let get = build_message::<IncrementerRef>(contract_acc_id.clone())
+        //         .call(|incrementer| incrementer.get());
+        //     let get_res = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
+        //     assert!(matches!(get_res.return_value(), 1));
+
+        //     // When
+        //     let new_code_hash = client
+        //         .upload("updated_incrementer", &ink_e2e::alice(), None)
+        //         .await
+        //         .expect("uploading `updated_incrementer` failed")
+        //         .code_hash;
+
+        //     let new_code_hash = new_code_hash.as_ref().try_into().unwrap();
+        //     let set_code = build_message::<IncrementerRef>(contract_acc_id.clone())
+        //         .call(|incrementer| incrementer.set_code(new_code_hash));
+
+        //     let _set_code_result = client
+        //         .call(&ink_e2e::alice(), set_code, 0, None)
+        //         .await
+        //         .expect("`set_code` failed");
+
+        //     // Then
+        //     // Note that our contract's `AccountId` (so `contract_acc_id`) has stayed the
+        //     // same between updates!
+        //     let inc = build_message::<IncrementerRef>(contract_acc_id.clone())
+        //         .call(|incrementer| incrementer.inc());
+
+        //     let _inc_result = client
+        //         .call(&ink_e2e::alice(), inc, 0, None)
+        //         .await
+        //         .expect("`inc` failed");
+
+        //     let get = build_message::<IncrementerRef>(contract_acc_id.clone())
+        //         .call(|incrementer| incrementer.get());
+        //     let get_res = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
+
+        //     // Remember, we updated our incrementer contract to increment by `4`.
+        //     assert!(matches!(get_res.return_value(), 5));
+
+        //     Ok(())
+        // }
     }
 }
