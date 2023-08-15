68e4432242b51a41d4fe79dfc1e130d0707e46bd
zaBuxx <wgo@belgoline.com>
2023-08-15 14:31:45 +0200

    create artefacts from build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -2,6 +2,9 @@
 
 set -eu
 
-cargo +nightly-2023-02-07 contract build --manifest-path contracts/issuer_staker/Cargo.toml --release
-cargo +nightly-2023-02-07 contract build --manifest-path contracts/oracle-validators/Cargo.toml --release
-cargo +nightly-2023-02-07 contract build --manifest-path contracts/validator_selector/Cargo.toml --release
+for contract in oracle_validators issuer_staker validator_selector; do
+    cargo +nightly-2023-02-07 contract build --manifest-path contracts/${contract}/Cargo.toml --release
+
+    cp -f target/ink/${contract}/${contract}.{contract,json} artefacts
+done
+
