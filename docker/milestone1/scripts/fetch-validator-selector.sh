20e4943d9703ed112ae82fcde4f4ce139bd0d736
zaBuxx <wgo@belgoline.com>
2023-08-26 15:40:34 +0200

    add offchain container

---
+++ docker/milestone1/scripts/fetch-validator-selector.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Fetch the liquid-staking repo from github
+
+. /tmp/functions.sh
+
+cd
+mkdir -p src
+cd src
+
+action "Fetching okAlice liquid-staking repo" git clone -q https://github.com/ok-Alice/liquid-staking.git
