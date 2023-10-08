d3eb222d15baea0bd6c410b465cbb1862b24b4b0
zaBuxx <wgo@belgoline.com>
2023-10-08 21:13:20 +0200

    tune dummy deploy

--- docker/milestone2/scripts/launch-dummy.sh
+++ docker/milestone2/scripts/launch-dummy.sh
@@ -12,6 +12,8 @@ cd src/liquid-staking/
 
 TARGET=artefacts/script/dummy-config.cfg
 
+echo "Waiting for $TARGET"
+
 # waiting for deploy container to drop the file
 while [ ! -f $TARGET ]; do
     sleep 5
@@ -23,15 +25,16 @@ echo "Found $TARGET"
 . $TARGET
 
 polkadot-js-api --ws ws://zombienet:9944 tx.assets.create $ASSET_ID ajYMsCKsEAhEvHpeA4XqsfiA9v1CdzZPrCfS6pEfeGHW9j8 100 --seed "//Alice" 
-polkadot-js-api --ws ws://zombienet:9944 tx.assets.setMetadata $ASSET_ID Token TOK 18   --seed "//Alice" 
-polkadot-js-api --ws ws://zombienet:9944 tx.assets.mint $ASSET_ID ajYMsCKsEAhEvHpeA4XqsfiA9v1CdzZPrCfS6pEfeGHW9j8 100000000000 --seed "//Alice" 
+polkadot-js-api --ws ws://zombienet:9944 tx.assets.setMetadata $ASSET_ID Token TOK 10   --seed "//Alice" 
+polkadot-js-api --ws ws://zombienet:9944 tx.assets.mint $ASSET_ID ajYMsCKsEAhEvHpeA4XqsfiA9v1CdzZPrCfS6pEfeGHW9j8 1000000000000 --seed "//Alice" 
 polkadot-js-api --ws ws://zombienet:9944 tx.assets.approveTransfer $ASSET_ID $CONTRACT 10000000000000 --seed "//Alice"
 
 polkadot-js-api --ws ws://zombienet:9944 tx.assets.create $LIQ_ASSET_ID ajYMsCKsEAhEvHpeA4XqsfiA9v1CdzZPrCfS6pEfeGHW9j8 100 --seed "//Alice" 
-polkadot-js-api --ws ws://zombienet:9944 tx.assets.setMetadata $LIQ_ASSET_ID LiquidToken lTOK 18  --seed "//Alice"
-polkadot-js-api --ws ws://zombienet:9944 tx.assets.mint $LIQ_ASSET_ID ajYMsCKsEAhEvHpeA4XqsfiA9v1CdzZPrCfS6pEfeGHW9j8 100000000000 --seed "//Alice" 
-polkadot-js-api --ws ws://zombienet:9944 tx.assets.approveTransfer $LIQ_ASSET_ID $CONTRACT 10000000000000 --seed "//Alice"
+polkadot-js-api --ws ws://zombienet:9944 tx.assets.setMetadata $LIQ_ASSET_ID LiquidToken lTOK 10  --seed "//Alice"
+polkadot-js-api --ws ws://zombienet:9944 tx.assets.mint $LIQ_ASSET_ID ajYMsCKsEAhEvHpeA4XqsfiA9v1CdzZPrCfS6pEfeGHW9j8 1000000000000 --seed "//Alice" 
+polkadot-js-api --ws ws://zombienet:9944 tx.assets.approveTransfer $LIQ_ASSET_ID $CONTRACT 1000000000000 --seed "//Alice"
 
+polkadot-js-api --ws ws://zombienet:9944 tx.balances.transfer $CONTRACT 1000000000  --seed "//Alice"
 
 echo "Dummy contract deployed at: $CONTRACT"
 echo 
