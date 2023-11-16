d01502a816d0c9a8b127560e13dc698afa0d5ba6
Kenneth Verbeure <kenneth.verbeure@excelra.com>
2023-11-16 20:12:50 +0100

    rewritten using headless ui

--- staking-frontend/src/ui-kit/Modal.tsx
+++ staking-frontend/src/ui-kit/Modal.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from "react";
-
+import React from "react";
+import { Dialog } from "@headlessui/react";
 import { Button } from "./buttons";
 import CloseIcon from "./icons/close-icon.svg";
 
@@ -11,47 +11,33 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ title, isOpen, onClose, children }) => {
-  const modalRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        modalRef.current &&
-        !modalRef.current.contains(event.target as Node)
-      ) {
-        onClose();
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [onClose]);
-
-  if (!isOpen) return null;
-
   return (
-    <div className="fixed inset-0 z-50 overflow-auto bg-gray-800 bg-opacity-50 flex items-start">
-      <div
-        ref={modalRef}
-        className="relative p-6 bg-white mx-auto mt-20 rounded-2xl w-1/3 flex-shrink-0"
-      >
-        <div className="flex justify-between items-center mb-4 -mt-6 -me-6">
-          <h2 className="text-2xl font-semibold">{title}</h2>
-          <span className="p-4 cursor-pointer" onClick={onClose}>
-            <CloseIcon className="h-8 w-8 font-semibold" />
-          </span>
-        </div>
-        <div className="mb-4 text-gray-800">{children}</div>
-        <div className="flex justify-end">
-          <Button onClick={onClose} variant="primary">
-            Close
-          </Button>
+    <Dialog
+      open={isOpen}
+      onClose={onClose}
+      className="fixed inset-0 z-50 overflow-auto"
+    >
+      <div className="flex items-start justify-center min-h-screen">
+        <Dialog.Overlay className="fixed inset-0 bg-gray-800 bg-opacity-50" />
+
+        <div className="relative p-6 bg-white mx-auto mt-20 rounded-2xl w-1/3 flex-shrink-0">
+          <Dialog.Title className="text-2xl font-semibold">
+            {title}
+          </Dialog.Title>
+          <button onClick={onClose} className="absolute top-0 right-0 p-4">
+            <CloseIcon className="h-8 w-8" />
+          </button>
+
+          <div className="mt-4 text-gray-800">{children}</div>
+
+          <div className="flex justify-end mt-4">
+            <Button onClick={onClose} variant="primary">
+              Close
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </Dialog>
   );
 };
 
